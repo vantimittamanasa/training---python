be2cf52d8efe98fdfab2cd27520df56a11206cc6
vantimittamanasa <vantimittamanasa@gmail.com>
2025-12-26 14:00:44 +0530

    Add files via upload

---
+++ main11.py
@@ -0,0 +1,12 @@
+n=int(input())
+org=n
+rev=0
+while n>0:
+    rem=n%10
+    rev=rev*10+rem
+    n=n//10
+    if rev==org:
+        print("palindrome")
+    else:
+            print("not palindrome")
+            
