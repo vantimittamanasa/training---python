24e9df259f607f9802c14fae6b38a21cfa13382e
vantimittamanasa <vantimittamanasa@gmail.com>
2025-12-27 11:40:01 +0530

    Add files via upload

---
+++ main21.py
@@ -0,0 +1,12 @@
+def moveZeroes(nums):
+    x=[]
+    y=[]
+    for i in range(len(nums)):
+        if nums[i]==0:
+            x.append(nums[i])
+        else:
+            y.append(nums[i])
+    z=y+x
+    print(z)
+nums=[0,1,0,3,12]
+moveZeroes(nums)
