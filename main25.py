9f11962692d2f8a4fd07b8fb2127d98eca421082
vantimittamanasa <vantimittamanasa@gmail.com>
2025-12-29 10:33:16 +0530

    Add files via upload

---
+++ main25.py
@@ -0,0 +1,13 @@
+def majorityElement(nums):
+    c = 0
+    candidate = 0
+    for num in nums:
+        if c == 0:
+            candidate = num
+            c = 1
+        elif candidate == num:
+            c += 1
+        else:
+            c -= 1
+    return candidate
+print(majorityElement([2, 2, 1, 1, 2, 2]))
