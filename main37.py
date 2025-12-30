4f77e665e3f519a2dc83fd4b1295db94f34b6478
vantimittamanasa <vantimittamanasa@gmail.com>
2025-12-30 15:13:05 +0530

    Add files via upload

---
+++ main37.py
@@ -0,0 +1,9 @@
+def linear_search(arr,target):
+    for i in range(len(arr)):
+        if arr[i]==target:
+           return i
+    return-1
+arr=list(map(int,input().split(":")))
+target=int(input())
+print(linear_search(arr,target))
+    
